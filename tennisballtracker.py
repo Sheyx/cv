6acad15d2cc3fe0a0a6aa68727e230625ee37cf0
Dmitry Kalmykov <sheyxtd@gmail.com>
2020-05-22 00:40:50 +0300

    Added color picker

--- tennisballtracker.py
+++ tennisballtracker.py
@@ -10,16 +10,23 @@ This is script for tracking ball
 
 
 import cv2
+import numpy as np
+
 
 cap = cv2.VideoCapture(0)
-cap.set(3,320) # set Width
-cap.set(4,240) # set Height
+cap.set(3,800) # set Width
+cap.set(4,600) # set Height
+
+hsv_min = np.array((28, 43, 136), np.uint8)
+hsv_max = np.array((49, 221, 255), np.uint8)
+
 
 while True:
     ret, img = cap.read()
-   
-
-    cv2.imshow('video',img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    thresh = cv2.inRange(hsv, hsv_min, hsv_max)
+    cv2.imshow('video',thresh)
+    
 
     k = cv2.waitKey(30) & 0xff
     if k == 27: # press 'ESC' to quit
