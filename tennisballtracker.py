5fe900c69813d4b9d759d5d7c72a410f0f5a911a
Dmitry Kalmykov <sheyxtd@gmail.com>
2020-05-23 19:09:03 +0300

    Add ball detecting and add rectangle

--- tennisballtracker.py
+++ tennisballtracker.py
@@ -14,18 +14,34 @@ import numpy as np
 
 
 cap = cv2.VideoCapture(0)
-cap.set(3,800) # set Width
+cap.set(3,600) # set Width
 cap.set(4,600) # set Height
 
-hsv_min = np.array((28, 43, 136), np.uint8)
-hsv_max = np.array((49, 221, 255), np.uint8)
+hsv_min = np.array((29, 54, 148), np.uint8)
+hsv_max = np.array((44, 216, 255), np.uint8)
 
 
 while True:
     ret, img = cap.read()
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #Фильтрация по диапазону цветов
     thresh = cv2.inRange(hsv, hsv_min, hsv_max)
-    cv2.imshow('video',thresh)
+    #Морфологическое преобразование - удаление шумов
+    st1 = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21), (10, 10))
+    st2 = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11), (5, 5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, st1)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, st2)
+    #Сглаживание
+    thresh = cv2.GaussianBlur(thresh, (5, 5), 2)
+    #Определение положения 
+    x,y,w,h = cv2.boundingRect(thresh)
+    #Определение минимального размера объекта
+    if w > 10 and h > 10:
+        cv2.rectangle(img,(x, y),(x+w, y+h),(0,0,255),3) 
+
+
+    cv2.imshow('result', img) 
+    
     
 
     k = cv2.waitKey(30) & 0xff
