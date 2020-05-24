ed67f171d493af78c552d8045a1fce62054c642a
Dmitry Kalmykov <sheyxtd@gmail.com>
2020-05-24 18:18:33 +0300

    Face recognition and view in new window

--- faceRecognition.py
+++ faceRecognition.py
@@ -6,16 +6,26 @@ Created on Wed May 24 17:30:20 2020
 @author: sheyx
 """
 import cv2
+import time
+import numpy as np
 
 faceCascade = cv2.CascadeClassifier('cascade/haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(1)
-cap.set(3, 800)  # set Width
-cap.set(4, 600)  # set Height
+#Подцепляем камеру
+cap = cv2.VideoCapture(0)
+#cap.set(3, 640)  # set Width
+#cap.set(4, 480)  # set Height
+ret, img = cap.read()
+time.sleep(1)
+h, w = img.shape[:2]
+roi_color = np.zeros((h, w, 3), np.uint8)
+
+t = 0
 
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.2,
@@ -28,7 +38,13 @@ while True:
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
 
+    dt = time.time() - t
+    t = time.time()
+    text = '%0.1f' % (1. / dt)
+    cv2.putText(img, text, (20, 20), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), 2)
+
     cv2.imshow('video', img)
+    cv2.imshow('face', roi_color)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:  # press 'ESC' to quit
